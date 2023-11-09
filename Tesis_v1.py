c3b9789833b76d57fe16f0602e92be9709bbaefe
gamadita3 <gamadita3@gmail.com>
2023-11-09 17:29:22 +0700

    tes commit raspi

--- Tesis_v1.py
+++ Tesis_v1.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import paho.mqtt.client as mqtt
 config = json.load(open(file="./config.json", encoding="utf-8"))
-
+#tescommitrasp
 #----------------------------MQTT SETUP-----------------------------------------#
 mqttClient = mqtt.Client()
 

ce4fcca816706cca35b48f896682fc271fd910a1
Yudha Gamadita <gamadita3@gmail.com>
2023-11-09 19:24:29 +0700

    Add yoloV8n inference to edge_v1

--- edge_v1.py
+++ edge_v1.py
@@ -1,8 +1,10 @@
 import json
 import time
 import cv2
+import random
 import numpy as np
 import paho.mqtt.client as mqtt
+from ultralytics import YOLO
 config = json.load(open(file="./config.json", encoding="utf-8"))
 
 #----------------------------MQTT SETUP-----------------------------------------#
@@ -36,22 +38,75 @@ def publish_image(image):
 #----------------------------CV2 SETUP-----------------------------------------#
 cap = cv2.VideoCapture(0)
 def setup_camera():
-    cap.set(3, config["res_width"])
-    cap.set(4, config["res_height"])
+    cap.set(3, config["frame_width"])
+    cap.set(4, config["frame_height"])
     return cap
+
+#----------------------------INFERENCE-----------------------------------------#
+coco_file = open("./coco.txt", "r")
+class_list = coco_file.read().split("\n")
+coco_file.close()
+
+model = YOLO("yolov8n.pt", "v8")
+detection_colors = []
+for i in range(len(class_list)):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    detection_colors.append((b, g, r))
+
+def inference(frame):
+    detecting = model.predict(source=[frame], conf=0.45, save=False)
+    DP = detecting[0].numpy()
+    print(DP)
+    
+    if len(DP) != 0:
+        for i in range(len(detecting[0])):
+            print(i)
+
+            boxes = detecting[0].boxes
+            box = boxes[i]  # returns one box
+            clsID = box.cls.numpy()[0]
+            conf = box.conf.numpy()[0]
+            bb = box.xyxy.numpy()[0]
+
+            cv2.rectangle(
+                frame,
+                (int(bb[0]), int(bb[1])),
+                (int(bb[2]), int(bb[3])),
+                detection_colors[int(clsID)],
+                3,
+            )
+
+            # Display class name and confidence
+            font = cv2.FONT_HERSHEY_COMPLEX
+            cv2.putText(
+                frame,
+                class_list[int(clsID)] + " " + str(round(conf, 3)) + "%",
+                (int(bb[0]), int(bb[1]) - 10),
+                font,
+                1,
+                (255, 255, 255),
+                2,
+            )
+    return frame
+    
 
 ########################################################################################
 
 def run():  
     while True:
         ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame. Exiting ...")
+            break
+        inference(frame)
         publish_image(frame)
-        #time.sleep(1)
 
 def main():
     initialize_mqtt()
-    #setup_camera()
-    #run()
+    setup_camera()
+    run()
 
 if __name__ == '__main__':
     main()

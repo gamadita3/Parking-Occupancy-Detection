f8840557c9de8cf108546b93d5420fbbe0ad3cf1
Yudha Gamadita <gamadita3@gmail.com>
2024-07-15 13:07:49 +0700

    fix load config on sampling.py

--- edge/sampling.py
+++ edge/sampling.py
@@ -1,4 +1,5 @@
 import time
+import json
 from mqtt_setup_edge import MQTTSetup
 
 
@@ -7,6 +8,10 @@ class Sampling:
         self.samplingConfig = self.load_config('../util/sampling_config.json')
         self.protocol = MQTTSetup()
         self.last_sent_time = time.time() - 300  
+        
+    def load_config(self, path):
+        with open(path, 'r', encoding='utf-8') as file:
+            return json.load(file)
         
     def save_sample(self, frame, currentTime):       
         if currentTime - self.last_sent_time >= self.samplingConfig["SAMPLE_INTERVAL"] :
